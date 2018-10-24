4b2947619f2602b5cd471f85ef8c19b20ab42261
wwyqianqian <1208331933@qq.com>
2018-10-24 23:07:42 +0800

    [feat]: overloadOperator.cpp

--- unit4-overload-operator/overloadOperator.cpp
+++ unit4-overload-operator/overloadOperator.cpp
@@ -5,28 +5,28 @@ class Rational {
     public:
         int numerator = 0;
         int denominator = 1;
-	Rational() {
+        Rational() {
         numerator = 0;
         denominator = 1;
-    	}
+        }
 
-	Rational(int numerator_ = 0, int denominator_ = 0) : numerator(numerator_), denominator(denominator_) { };
-	Rational operator+(const Rational & add); // 重载为成员函数
-	Rational operator-(const Rational & subtract); // 重载为成员函数
+        Rational(int numerator_ = 0, int denominator_ = 0) : numerator(numerator_), denominator(denominator_) { };
+        Rational operator+(const Rational & add); // 重载为成员函数
+        Rational operator-(const Rational & subtract); // 重载为成员函数
 };
 
 Rational Rational::operator+(const Rational & add) { // 重载为成员函数
-	return Rational(numerator + add.numerator, denominator + add.denominator);
+    return Rational(numerator + add.numerator, denominator + add.denominator);
 }
 Rational Rational::operator-(const Rational & subtract) { // 重载为成员函数
-	return Rational(numerator - subtract.numerator, denominator - subtract.denominator);
+    return Rational(numerator - subtract.numerator, denominator - subtract.denominator);
 }
 
 Rational operator*(const Rational & a, const Rational & b) { // 重载为普通的函数
-	return Rational(a.numerator * b.numerator, a.denominator * b.denominator);
+    return Rational(a.numerator * b.numerator, a.denominator * b.denominator);
 }
 Rational operator/(const Rational & a, const Rational & b) { // 重载为普通的函数
-	return Rational(a.numerator / b.numerator, a.denominator / b.denominator);
+    return Rational(a.numerator / b.numerator, a.denominator / b.denominator);
 }
 
 
